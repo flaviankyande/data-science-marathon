7d91a914a69e6964c2f8cee8aaef5508b27e12d7
Flavian Kyande <kyandeflavian@gmail.com>
2022-03-29 23:23:33 +0300

    Create fahrenheit_celcius.py

---
+++ day-1-2/fahrenheit_celcius.py
@@ -0,0 +1,8 @@
+# take the temperature in fahrenheit
+fahrenheit = input('Input temperature in Fahrenheit = ')
+
+# formula to convert temperature to celcius
+celcius = (float(fahrenheit) - 32) * 5 / 9
+
+#return temperature
+print('Temperature in celcius = ', celcius)
